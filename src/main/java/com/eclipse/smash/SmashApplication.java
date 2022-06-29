454cd5ecefe038a991364e9af3777606bea18908
eclipse <projects.eclipse@gmail.com>
2022-06-29 17:43:30 +0200

    Launch of project smash ultimate api

---
+++ src/main/java/com/eclipse/smash/SmashApplication.java
@@ -0,0 +1,11 @@
+package com.eclipse.smash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashApplication {
+
+    public static void main(String[] args) { SpringApplication.run(SmashApplication.class, args);}
+
+}
